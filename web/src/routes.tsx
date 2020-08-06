90cc22328bf30b99690ffff4d9413607856f649f
SkyHeads <skyheads@live.com>
2020-08-06 20:36:39 -0300

    teacher list page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Landing from './pages/Landing';
+import TeacherList from './pages/TeacherList';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Landing} />
+      <Route exact path="/give-classes" component={TeacherList} />
     </BrowserRouter>
   );
 }
