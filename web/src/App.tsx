8f39e3a3541b35766593d8b9e293f681d58265b2
SkyHeads <skyheads@live.com>
2020-08-06 19:00:14 -0300

    install styled components/create global style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App = () => {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
