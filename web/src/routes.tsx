66cb8957e757ca06c547916016920ea2cf054a66
SkyHeads <skyheads@live.com>
2020-08-06 22:14:23 -0300

    create teacher form page and use in routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
+import TeacherForm from './pages/TeacherForm';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Landing} />
-      <Route exact path="/give-classes" component={TeacherList} />
+      <Route exact path="/give-classes" component={TeacherForm} />
+      <Route exact path="/study" component={TeacherList} />
     </BrowserRouter>
   );
 };
