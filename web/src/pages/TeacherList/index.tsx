1d11285b15610a9e886da069ff09e575f63a7ea2
SkyHeads <skyheads@live.com>
2020-08-07 00:59:16 -0300

    teacher item component and styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import { PageTeacherList } from './styles';
 
 const TeacherList: React.FC = () => {
@@ -28,38 +28,10 @@ const TeacherList: React.FC = () => {
         </form>
       </PageHeader>
       <main>
-        <article className="teacher-item">
-          <header>
-            <img
-              src="https://avatars3.githubusercontent.com/u/43793592?s=460&u=977cc37f6e87bcc029d8efc6d89ca7e5a5d55de9&v=4"
-              alt="Avatar"
-            />
-            <div>
-              <strong>Bruno Fernandes</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br />
-            <br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida de
-            pessoas através de experiências. Mais de 200.000 pessoas já passaram
-            por uma das minhas explosões.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </PageTeacherList>
   );
