5d01961da2cea5c9ccedc61504bb887b23c3fcf4
SkyHeads <skyheads@live.com>
2020-11-13 19:10:59 -0300

    install eslint + prettier + editorconfig

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Hello Proffy!</h1>
-  );
-}
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <div />;
+};
 
 export default App;
