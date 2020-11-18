300aa4313ebaac7879b0c122dec2cb927aacf6dc
SkyHeads <skyheads@live.com>
2020-11-18 18:07:36 -0300

    filter classes route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,7 @@ import ClassesController from './controllers/ClassesController';
 const routes = Router();
 const classesControllers = new ClassesController();
 
+routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
 export default routes;
