c09ef16543964903e1aca3afc665fd632632103a
SkyHeads <skyheads@live.com>
2020-11-19 18:31:12 -0300

    initial setup expo mobile with assets for project

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <Text>Hello NLW</Text>
+      <StatusBar style="light" />
+    </>
+  );
+}
