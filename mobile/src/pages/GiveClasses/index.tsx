2339036b1fd0b7966a5783b8f8a6d96395ed27b6
SkyHeads <skyheads@live.com>
2020-11-20 01:51:19 -0300

    give classes page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
 
-const GiveClasses: React.FC = () => (
-  <View>
-    <Text>GiveClasses</Text>
-  </View>
-);
+import giveClassesBgImg from '../../assets/images/give-classes-background.png';
+
+const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  function handleNavigateToLanding() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImg}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+      <RectButton onPress={handleNavigateToLanding} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
+  );
+};
 
 export default GiveClasses;
